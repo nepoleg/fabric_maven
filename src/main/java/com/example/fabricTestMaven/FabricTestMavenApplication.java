257745c8ba1abe6c0d0059e7549d36dbc07a89c5
oleg <oleg@oleg.com>
2018-02-04 20:23:07 +0300

    first commit

---
+++ src/main/java/com/example/fabricTestMaven/FabricTestMavenApplication.java
@@ -0,0 +1,12 @@
+package com.example.fabricTestMaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabricTestMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabricTestMavenApplication.class, args);
+	}
+}
